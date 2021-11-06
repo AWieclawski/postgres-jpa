15d30bed22c9d565a1979ad7cc3f6b1898032aee
AWieclawski <a_wieclawski@poczta.pl>
2021-11-06 16:27:19 +0100

    PostgresJpaApplication optimized for WAR deployment

--- src/main/java/edu/awieclawski/postgresjpa/PostgresJpaApplication.java
+++ src/main/java/edu/awieclawski/postgresjpa/PostgresJpaApplication.java
@@ -2,12 +2,28 @@ package edu.awieclawski.postgresjpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * 
+ * edu.awieclawski.postgresjpa.PostgresJpaApplication
+ * 
+ * @author AWieclawski
+ * 
+ * content optimized for WAR deployment from only one class.
+ *
+ */
 @SpringBootApplication
-public class PostgresJpaApplication {
+public class PostgresJpaApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PostgresJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PostgresJpaApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(PostgresJpaApplication.class);
+    }
 
 }
