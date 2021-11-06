7cdc7538d23cdb4ef2ca6a1381c9983e8b65bf17
AWieclawski <a_wieclawski@poczta.pl>
2021-11-06 15:38:26 +0100

    git init

---
+++ src/test/java/edu/awieclawski/postgresjpa/PostgresJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.awieclawski.postgresjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgresJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
